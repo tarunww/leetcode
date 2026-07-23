c980a18f70e744b1f139b51a2d71c08c842ca861
tarunww <tarunsb24@gmail.com>
2026-07-23 14:55:06 +0530

    Time: 0 ms (100%), Space: 11.7 MB (85.11%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int writeIndex = 0;
+      
+        for (int currentElement : nums) {
+            if (currentElement != val) {
+                nums[writeIndex] = currentElement;
+                writeIndex++;
+            }
+        }
+      
+        return writeIndex;
+    }
+};
